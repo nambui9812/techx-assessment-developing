7c8af3ed7bbaf9d1e5b11953ef3c817a00deb0e6
nambui9812 <buihainam9812@gmail.com>
2024-04-03 22:08:38 +0700

    add headers middleware and tests for workspaces, refactor code

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
 mod users;
 mod workspaces;
 
-use axum::{
-    routing::{delete, get}, Router
-};
+use axum::{middleware::from_fn, routing::{delete, get}, Router};
 
 use tokio::net::TcpListener;
 
 use sqlx::postgres::PgPoolOptions;
 
 use users::{get_users, create_user, delete_user};
-use workspaces::{get_workspaces, get_workspace_by_id, create_workspace, update_workspace_status, };
+use workspaces::{check_owner_id_middleware, create_workspace, get_workspace_by_id, get_workspaces, update_workspace_status };
 
 #[tokio::main]
 async fn main() {
@@ -25,14 +23,15 @@ async fn main() {
         .await
         .expect("Cannot connect to database");
 
-    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
 
     let app = Router::new()
+        .route("/workspaces", get(get_workspaces).post(create_workspace))
+        .route("/workspaces/:workspace_id", get(get_workspace_by_id).put(update_workspace_status))
+        .layer(from_fn(check_owner_id_middleware))
         .route("/", get(root))
         .route("/users", get(get_users).post(create_user))
         .route("/users/:user_id", delete(delete_user))
-        .route("/workspaces", get(get_workspaces).post(create_workspace))
-        .route("/workspaces/:workspace_id", get(get_workspace_by_id).put(update_workspace_status))
         .with_state(db_pool);
 
     axum::serve(listener, app).await.unwrap();
--- src/workspaces/mod.rs
+++ src/workspaces/mod.rs
@@ -1,5 +1,5 @@
 use axum::{
-    extract::{Path, State}, http::{HeaderMap, StatusCode}, Json
+    extract::{Path, Request, State}, http::{HeaderMap, StatusCode}, middleware::Next, response::IntoResponse, Extension, Json
 };
 
 use serde::{Serialize, Deserialize};
@@ -39,14 +39,37 @@ pub struct UpdateWorkspaceStatusReq {
     pub status: Status
 }
 
-pub async fn get_workspaces(
+pub async fn check_owner_id_middleware(
     headers: HeaderMap,
+    mut req: Request,
+    next: Next
+) -> Result<impl IntoResponse, (StatusCode, String)> {
+    let owner_header = headers.get("x-owner");
+    let owner_id: i32;
+
+    match owner_header {
+        Some(h) => owner_id = h.to_str().unwrap().to_owned().parse().map_err(|_| {
+            (
+                StatusCode::BAD_REQUEST,
+                json!({ "success": false, "message": "Invalid header" }).to_string()
+            )
+        })?,
+        None => return Err((
+            StatusCode::BAD_REQUEST,
+            json!({ "success": false, "message": "Invalid header" }).to_string()
+        ))
+    };
+
+    req.extensions_mut().insert(owner_id);
+    Ok(next.run(req).await)
+}
+
+pub async fn get_workspaces(
+    Extension(owner_id): Extension<i32>,
     State(pool): State<PgPool>,
 ) -> Result<(StatusCode, String), (StatusCode, String)> {
-    let onwer_id: i32 = headers.get("x-owner").unwrap().to_str().unwrap().to_owned().parse().expect("Invalid headers");
-
     let workspaces = sqlx::query_as::<_, WorkspaceRow>("SELECT * FROM workspaces WHERE owner_id = $1")
-        .bind(onwer_id)
+        .bind(owner_id)
         .fetch_all(&pool)
         .await
         .map_err(|e| {
@@ -63,15 +86,13 @@ pub async fn get_workspaces(
 }
 
 pub async fn get_workspace_by_id(
-    headers: HeaderMap,
+    Extension(owner_id): Extension<i32>,
     State(pool): State<PgPool>,
     Path(workspace_id): Path<i32>,
 ) -> Result<(StatusCode, String), (StatusCode, String)> {
-    let onwer_id: i32 = headers.get("x-owner").unwrap().to_str().unwrap().to_owned().parse().expect("Invalid headers");
-
     let workspace = sqlx::query_as::<_, WorkspaceRow>("SELECT * FROM workspaces WHERE id = $1 AND owner_id = $2")
         .bind(workspace_id)
-        .bind(onwer_id)
+        .bind(owner_id)
         .fetch_one(&pool)
         .await
         .map_err(|e| {
@@ -88,17 +109,15 @@ pub async fn get_workspace_by_id(
 }
 
 pub async fn create_workspace(
-    headers: HeaderMap,
+    Extension(owner_id): Extension<i32>,
     State(pool): State<PgPool>,
     Json(body): Json<CreateWorkspaceReq>
 ) -> Result<(StatusCode, String), (StatusCode, String)> {
-    let onwer_id: i32 = headers.get("x-owner").unwrap().to_str().unwrap().to_owned().parse().expect("Invalid headers");
-
     let workspace = sqlx::query_as::<_, WorkspaceRow>("INSERT INTO workspaces(name, description, status, owner_id) VALUES ($1, $2, $3, $4) RETURNING *")
         .bind(body.name)
         .bind(body.description)
         .bind(Status::Active)
-        .bind(onwer_id)
+        .bind(owner_id)
         .fetch_one(&pool)
         .await
         .map_err(|e| {
@@ -115,17 +134,15 @@ pub async fn create_workspace(
 }
 
 pub async fn update_workspace_status(
-    headers: HeaderMap,
+    Extension(owner_id): Extension<i32>,
     State(pool): State<PgPool>,
     Path(workspace_id): Path<i32>,
     Json(body): Json<UpdateWorkspaceStatusReq>
 ) -> Result<(StatusCode, String), (StatusCode, String)> {
-    let onwer_id: i32 = headers.get("x-owner").unwrap().to_str().unwrap().to_owned().parse().expect("Invalid headers");
-
     let workspace = sqlx::query_as::<_, WorkspaceRow>("UPDATE workspaces SET status = $1 WHERE id = $2 AND owner_id = $3 RETURNING *")
         .bind(body.status)
         .bind(workspace_id)
-        .bind(onwer_id)
+        .bind(owner_id)
         .fetch_one(&pool)
         .await
         .map_err(|e| {
@@ -136,13 +153,13 @@ pub async fn update_workspace_status(
         })?;
     
     Ok((
-        StatusCode::CREATED,
+        StatusCode::OK,
         json!({ "success": true, "data": workspace }).to_string()
     ))
 }
 
 
-// For easier development process
+// ---------------------- For easier development process ----------------------
 
 pub async fn _get_workspaces(
     State(pool): State<PgPool>,
@@ -226,4 +243,197 @@ pub async fn _delete_workspace(
         StatusCode::OK,
         json!({ "success": true }).to_string()
     ))
+}
+
+// ---------------------- For tests ----------------------
+
+#[cfg(test)]
+mod tests {
+    use axum::{body::Body, http::{Method, Request}, middleware::from_fn, routing::get, Router};
+    use serde_json::Value;
+    use sqlx::{postgres::PgPoolOptions, PgPool};
+    use tower::ServiceExt;          // allow 'oneshot' method
+    use http_body_util::BodyExt;    // allow 'collect' method
+    use super::*;
+
+    async fn create_connection_pool() -> PgPool {
+        dotenvy::dotenv().expect("Cannot access .env file");
+
+        let database_url: String = std::env::var("DATABASE_URL")
+            .expect("DATABASE_URL not found in .env file");
+
+        let pool = PgPoolOptions::new()
+            .max_connections(5)
+            .connect(&database_url)
+            .await
+            .expect("Cannot connect to database");
+        
+        return pool;
+    }
+
+    async fn app() -> Router {
+        let pool = create_connection_pool().await;
+
+        let app: Router = Router::new()
+            .route("/workspaces", get(get_workspaces).post(create_workspace))
+            .route("/workspaces/:workspace_id", get(get_workspace_by_id).put(update_workspace_status))
+            .layer(from_fn(check_owner_id_middleware))
+            .with_state(pool);
+
+        return app;
+    }
+
+    #[tokio::test]
+    async fn test_database_connectivity() {
+        let pool = create_connection_pool().await;
+
+        assert_eq!(pool.is_closed(), false);
+    }
+
+    #[tokio::test]
+    async fn test_create_workspace_successfully() {
+        let app: Router = app().await;
+
+        let req = Request::builder()
+            .method(Method::POST)
+            .uri("/workspaces")
+            .header("content-type", "application/json")
+            .header("x-owner", 1)
+            .body(Body::from(
+                r#"{
+                    "name": "w1 of u1",
+                    "description": "w1 of u1"
+                }"#,
+            ))
+            .unwrap();
+        
+        let res = app
+            .oneshot(req)
+            .await
+            .unwrap();
+
+        assert_eq!(res.status(), StatusCode::CREATED);
+
+        let body_bytes = res.into_body().collect().await.unwrap().to_bytes();
+        let body: Value = serde_json::from_slice(&body_bytes).unwrap();
+
+        assert_eq!(body["success"], true);
+        assert_eq!(body["data"]["owner_id"], 1);
+    }
+
+    #[tokio::test]
+    async fn test_create_workspace_failed_due_to_lack_of_ower_id_header() {
+        let app: Router = app().await;
+
+        let req = Request::builder()
+            .method(Method::POST)
+            .uri("/workspaces")
+            .header("content-type", "application/json")
+            .body(Body::from(
+                r#"{
+                    "name": "w1 of u1",
+                    "description": "w1 of u1"
+                }"#,
+            ))
+            .unwrap();
+        
+        let res = app
+            .oneshot(req)
+            .await
+            .unwrap();
+
+        assert_eq!(res.status(), StatusCode::BAD_REQUEST);
+    }
+
+    #[tokio::test]
+    async fn test_create_workspace_failed_due_to_lack_of_name_field() {
+        let app: Router = app().await;
+
+        let req = Request::builder()
+            .method(Method::POST)
+            .uri("/workspaces")
+            .header("content-type", "application/json")
+            .header("x-owner", 1)
+            .body(Body::from(
+                r#"{
+                    "description": "w1 of u1"
+                }"#,
+            ))
+            .unwrap();
+        
+        let res = app
+            .oneshot(req)
+            .await
+            .unwrap();
+
+        assert_eq!(res.status(), StatusCode::UNPROCESSABLE_ENTITY);
+    }
+
+    #[tokio::test]
+    async fn test_get_workspaces() {
+        let app: Router = app().await;
+
+        let req = Request::builder()
+            .method(Method::GET)
+            .uri("/workspaces")
+            .header("x-owner", 1)
+            .body(Body::empty())
+            .unwrap();
+        
+        let res = app
+            .oneshot(req)
+            .await
+            .unwrap();
+
+        assert_eq!(res.status(), StatusCode::OK);
+    }
+
+    #[tokio::test]
+    async fn test_get_workspace_by_id() {
+        let app: Router = app().await;
+
+        let req = Request::builder()
+            .method(Method::GET)
+            .uri("/workspaces/1")
+            .header("x-owner", 1)
+            .body(Body::empty())
+            .unwrap();
+        
+        let res = app
+            .oneshot(req)
+            .await
+            .unwrap();
+
+        assert_eq!(res.status(), StatusCode::OK);
+    }
+
+    #[tokio::test]
+    async fn test_update_workspace_status_successfully() {
+        let app: Router = app().await;
+
+        let req = Request::builder()
+            .method(Method::PUT)
+            .uri("/workspaces/1")
+            .header("content-type", "application/json")
+            .header("x-owner", 1)
+            .body(Body::from(
+                r#"{
+                    "status": "Expired"
+                }"#,
+            ))
+            .unwrap();
+        
+        let res = app
+            .oneshot(req)
+            .await
+            .unwrap();
+
+        assert_eq!(res.status(), StatusCode::OK);
+
+        let body_bytes = res.into_body().collect().await.unwrap().to_bytes();
+        let body: Value = serde_json::from_slice(&body_bytes).unwrap();
+
+        assert_eq!(body["success"], true);
+        assert_eq!(body["data"]["status"], "Expired");
+    }
 }
